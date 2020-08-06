7f829efb3946040fed5780913e1193782d9be2e3
alswns <49472611+alswns@users.noreply.github.com>
2020-08-07 01:23:13 +0900

    add17

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def index():
     return 'Hello Flask'
 if __name__=='main':
     print('run Server')
-    app.run(host='0.0.0.0',port=80, debug=True) 
+    app.run(host='127.0.0.1',port=3000, debug=True) 
