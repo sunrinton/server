22faa7f01a6295fc999ba2b4e8b8cddbbe17f40b
alswns <49472611+alswns@users.noreply.github.com>
2020-08-07 00:25:19 +0900

    add8

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask,request,jsonify,abort
 import flask
-app = Flask(__name__,static_url_path='/static')
+app = Flask(__name__)
 @app.route('/')
 def index():
-    print(flask.__version__)
     return 'Hello Flask'
 
 app.run(host='127.0.0.1',port=80, debug=True) 
